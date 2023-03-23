c929f6f2674e64ee967d711931495eb41d1d0eb3
Miroslav Bogatyrev <miroslav.b@ecomsolutions.net>
2023-03-23 09:59:29 +0100

    some minor fix

--- tests/MartianRobots.GPT.Tests/MartianRobots.GPT.Tests/Solver/MartianRobotsSolverTests.cs
+++ tests/MartianRobots.GPT.Tests/MartianRobots.GPT.Tests/Solver/MartianRobotsSolverTests.cs
@@ -3,65 +3,61 @@ using MartianRobots.GPT.Solver;
 
 namespace MartianRobots.GPT.Tests.Solver
 {
-    namespace MartianRobots.Tests.Solver
+    public class MartianRobotsSolverTests
     {
-        public class MartianRobotsSolverTests
+        [Fact]
+        public void TestSampleInput()
         {
-            [Fact]
-            public void TestSampleInput()
-            {
-                string input = "5 3\r\n1 1 E\r\nRFRFRFRF\r\n3 2 N\r\nFRRFLLFFRRFLL\r\n0 3 W\r\nLLFFFLFLFL";
-                string expectedOutput = "1 1 E\n3 3 N LOST\n2 3 S";
+            string input = "5 3\r\n1 1 E\r\nRFRFRFRF\r\n3 2 N\r\nFRRFLLFFRRFLL\r\n0 3 W\r\nLLFFFLFLFL";
+            string expectedOutput = "1 1 E\n3 3 N LOST\n2 3 S";
 
-                string result = MartianRobotsSolver.Solve(input);
+            string result = MartianRobotsSolver.Solve(input);
 
-                Assert.Equal(expectedOutput, result);
-            }
+            Assert.Equal(expectedOutput, result);
+        }
 
-            [Fact]
-            public void TestEmptyInput()
-            {
-                string input = "";
-                string expectedOutput = "";
+        [Fact]
+        public void TestEmptyInput()
+        {
+            string input = "";
+            string expectedOutput = "";
 
-                string result = MartianRobotsSolver.Solve(input);
+            string result = MartianRobotsSolver.Solve(input);
 
-                Assert.Equal(expectedOutput, result);
-            }
+            Assert.Equal(expectedOutput, result);
+        }
 
-            [Fact]
-            public void TestRobotDoesNotMove()
-            {
-                string input = "5 3\r\n1 1 E\r\n";
-                string expectedOutput = "1 1 E";
+        [Fact]
+        public void TestRobotDoesNotMove()
+        {
+            string input = "5 3\r\n1 1 E\r\n";
+            string expectedOutput = "1 1 E";
 
-                string result = MartianRobotsSolver.Solve(input);
+            string result = MartianRobotsSolver.Solve(input);
 
-                Assert.Equal(expectedOutput, result);
-            }
+            Assert.Equal(expectedOutput, result);
+        }
 
-            [Fact]
-            public void TestRobotMoveOffGrid()
-            {
-                string input = "5 3\r\n1 1 N\r\nFFFFF";
-                string expectedOutput = "1 3 N LOST";
+        [Fact]
+        public void TestRobotMoveOffGrid()
+        {
+            string input = "5 3\r\n1 1 N\r\nFFFFF";
+            string expectedOutput = "1 3 N LOST";
 
-                string result = MartianRobotsSolver.Solve(input);
+            string result = MartianRobotsSolver.Solve(input);
 
-                Assert.Equal(expectedOutput, result);
-            }
+            Assert.Equal(expectedOutput, result);
+        }
 
-            [Fact]
-            public void TestRobotMoveOffGridAndLeaveScent()
-            {
-                string input = "5 3\r\n1 1 N\r\nFFFFF\r\n1 1 N\r\nFFFFF";
-                string expectedOutput = "1 3 N LOST\n1 3 N";
+        [Fact]
+        public void TestRobotMoveOffGridAndLeaveScent()
+        {
+            string input = "5 3\r\n1 1 N\r\nFFFFF\r\n1 1 N\r\nFFFFF";
+            string expectedOutput = "1 3 N LOST\n1 3 N";
 
-                string result = MartianRobotsSolver.Solve(input);
+            string result = MartianRobotsSolver.Solve(input);
 
-                Assert.Equal(expectedOutput, result);
-            }
+            Assert.Equal(expectedOutput, result);
         }
     }
-
 }
